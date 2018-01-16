9a27dc355f5e68adc4a798ed846efe90140820ba
equipo <hacsor@gmail.com>
2018-01-16 16:11:29 -0500

    Nuevos modulos

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+
+import { Graficas1Component } from './graficas1/graficas1.component';
+import { ProgressComponent } from './progress/progress.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { PagesComponent } from './pages.component';
+
+// Modulos
+import { SharedModule } from '../shared/shared.module';
+
+
+// Rutas
+import { PAGES_ROUTES } from './pages.routes';
+
+
+
+
+@NgModule({
+    declarations: [
+        DashboardComponent,
+        ProgressComponent,
+        Graficas1Component,
+        PagesComponent
+    ],
+    exports: [
+        DashboardComponent,
+        ProgressComponent,
+        Graficas1Component,
+        PagesComponent
+    ],
+    imports: [
+        SharedModule,
+        PAGES_ROUTES
+    ]
+})
+
+export class PageModule {  }
